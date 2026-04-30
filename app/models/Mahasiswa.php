227586882e61f2715fe607d7f1123ad77fa659f6
ciciieuy <shahlaqeysha@MacBook-Air-Noor.local>
2026-04-30 16:02:17 +0800

    Sesi 5 - tambah method update dan delete model

--- app/models/Mahasiswa.php
+++ app/models/Mahasiswa.php
@@ -51,4 +51,52 @@ class Mahasiswa
             ':status_id' => 1
         ]);
     }
+    public function find($id)
+{
+    $query = "SELECT * FROM mahasiswa WHERE id = :id LIMIT 1";
+
+    $stmt = $this->db->prepare($query);
+    $stmt->execute([
+        ':id' => $id
+    ]);
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+public function update($id, $data)
+{
+    $query = "UPDATE mahasiswa SET
+        npm = :npm,
+        nama_lengkap = :nama_lengkap,
+        fakultas = :fakultas,
+        jurusan = :jurusan,
+        tempat_lahir = :tempat_lahir,
+        tanggal_lahir = :tanggal_lahir,
+        jenis_kelamin = :jenis_kelamin
+        WHERE id = :id";
+
+    $stmt = $this->db->prepare($query);
+
+    return $stmt->execute([
+        ':id' => $id,
+        ':npm' => $data['npm'],
+        ':nama_lengkap' => $data['nama_lengkap'],
+        ':fakultas' => $data['fakultas'],
+        ':jurusan' => $data['jurusan'],
+        ':tempat_lahir' => $data['tempat_lahir'],
+        ':tanggal_lahir' => $data['tanggal_lahir'],
+        ':jenis_kelamin' => $data['jenis_kelamin']
+    ]);
+}
+
+public function delete($id)
+{
+    $query = "DELETE FROM mahasiswa WHERE id = :id";
+
+    $stmt = $this->db->prepare($query);
+
+    return $stmt->execute([
+        ':id' => $id
+    ]);
+}
 }
