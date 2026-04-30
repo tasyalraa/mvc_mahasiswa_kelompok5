5d2af92c9cbaa9ab03d4935a957fc3f293955557
ciciieuy <shahlaqeysha@MacBook-Air-Noor.local>
2026-04-30 15:12:27 +0800

    Sesi 4 - tambah method create mahasiswa

--- app/models/Mahasiswa.php
+++ app/models/Mahasiswa.php
@@ -18,4 +18,37 @@ class Mahasiswa
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function findByNpm($npm)
+    {
+        $query = "SELECT * FROM mahasiswa WHERE npm = :npm LIMIT 1";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([
+            ':npm' => $npm
+        ]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($data)
+    {
+        $query = "INSERT INTO mahasiswa 
+            (npm, nama_lengkap, fakultas, jurusan, tempat_lahir, tanggal_lahir, jenis_kelamin, status_id)
+            VALUES 
+            (:npm, :nama_lengkap, :fakultas, :jurusan, :tempat_lahir, :tanggal_lahir, :jenis_kelamin, :status_id)";
+
+        $stmt = $this->db->prepare($query);
+
+        return $stmt->execute([
+            ':npm' => $data['npm'],
+            ':nama_lengkap' => $data['nama_lengkap'],
+            ':fakultas' => $data['fakultas'],
+            ':jurusan' => $data['jurusan'],
+            ':tempat_lahir' => $data['tempat_lahir'],
+            ':tanggal_lahir' => $data['tanggal_lahir'],
+            ':jenis_kelamin' => $data['jenis_kelamin'],
+            ':status_id' => 1
+        ]);
+    }
 }
