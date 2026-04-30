e171a3064e0ea45b32fa8caad60f7c913fb01ac4
ciciieuy <shahlaqeysha@MacBook-Air-Noor.local>
2026-04-30 15:11:50 +0800

    Sesi 4 - tambah flash message dan redirect

--- core/Controller.php
+++ core/Controller.php
@@ -27,4 +27,29 @@ class Controller
         echo "Model '$model' tidak ditemukan.";
         return null;
     }
+
+    public function setFlash($type, $message)
+    {
+        $_SESSION['flash'] = [
+            'type' => $type,
+            'message' => $message
+        ];
+    }
+
+    public function flash()
+    {
+        if (isset($_SESSION['flash'])) {
+            $flash = $_SESSION['flash'];
+            unset($_SESSION['flash']);
+            return $flash;
+        }
+
+        return null;
+    }
+
+    public function redirect($path)
+    {
+        header('Location: ' . BASEURL . '/' . ltrim($path, '/'));
+        exit;
+    }
 }
